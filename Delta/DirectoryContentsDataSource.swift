46bf22035963a057cb6072b290dae75d4e49c25f
Riley Testut <Riley@Rileys-MacBook-Pro.local>
2015-03-08 13:35:02 -0700

    Lists files in Documents directory, automatically updates when files are added or removed

---
+++ Delta/DirectoryContentsDataSource.swift
@@ -0,0 +1,121 @@
+//
+//  DirectoryContentsDataSource.swift
+//  Delta
+//
+//  Created by Riley Testut on 3/8/15.
+//  Copyright (c) 2015 Riley Testut. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+public extension DirectoryContentsDataSource
+{
+    func URLAtIndexPath(indexPath: NSIndexPath) -> NSURL
+    {
+        let URL = self.directoryContents[indexPath.row]
+        return URL
+    }
+}
+
+public class DirectoryContentsDataSource: NSObject
+{
+    public let directoryURL: NSURL
+    public var tableViewCellIdentifier: String = "Cell"
+    
+    public var contentsUpdateHandler: (Void -> Void)?
+    public var cellConfigurationBlock: ((UITableViewCell, NSIndexPath, NSURL) -> Void)?
+    
+    private let fileDescriptor: Int32
+    private let directoryMonitorDispatchQueue: dispatch_queue_t
+    private let directoryMonitorDispatchSource: dispatch_source_t!
+    
+    private var directoryContents: [NSURL]
+    
+    required public init?(directoryURL: NSURL)
+    {
+        self.directoryURL = directoryURL
+        self.fileDescriptor = open(self.directoryURL.fileSystemRepresentation, O_EVTONLY)
+        
+        self.directoryMonitorDispatchQueue = dispatch_queue_create("com.rileytestut.DirectoryContentsDataSource", DISPATCH_QUEUE_SERIAL)
+        self.directoryMonitorDispatchSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_VNODE, UInt(self.fileDescriptor), DISPATCH_VNODE_WRITE, self.directoryMonitorDispatchQueue)
+        
+        self.directoryContents = [NSURL]()
+        
+        super.init()        
+        
+        if self.fileDescriptor < 0
+        {
+            return nil
+        }
+        
+        if self.directoryMonitorDispatchSource == nil
+        {
+            close(self.fileDescriptor);
+            
+            return nil
+        }
+        
+        dispatch_source_set_event_handler(self.directoryMonitorDispatchSource, {
+            self.didUpdateDirectoryContents()
+        });
+        
+        dispatch_source_set_cancel_handler(self.directoryMonitorDispatchSource, {
+            close(self.fileDescriptor);
+        });
+        
+        dispatch_resume(self.directoryMonitorDispatchSource);
+        
+        self.didUpdateDirectoryContents()
+    }
+    
+    deinit
+    {
+        if self.fileDescriptor >= 0
+        {
+            close(self.fileDescriptor);
+        }
+        
+        if self.directoryMonitorDispatchSource != nil
+        {
+            dispatch_source_cancel(self.directoryMonitorDispatchSource);
+        }
+    }
+}
+
+private extension DirectoryContentsDataSource
+{
+    func didUpdateDirectoryContents()
+    {
+        var error: NSError? = nil
+        var contents = NSFileManager.defaultManager().contentsOfDirectoryAtURL(self.directoryURL, includingPropertiesForKeys: nil, options:NSDirectoryEnumerationOptions.SkipsSubdirectoryDescendants | NSDirectoryEnumerationOptions.SkipsHiddenFiles, error: &error) as! [NSURL]?
+        
+        if let contents = contents
+        {
+            self.directoryContents = contents
+        }
+        else
+        {
+            println(error?.userInfo)
+        }
+        
+        self.contentsUpdateHandler?()
+    }
+}
+
+extension DirectoryContentsDataSource: UITableViewDataSource
+{
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return self.directoryContents.count
+    }
+    
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let tableViewCell = tableView.dequeueReusableCellWithIdentifier(self.tableViewCellIdentifier, forIndexPath: indexPath) as! UITableViewCell
+        
+        self.cellConfigurationBlock?(tableViewCell, indexPath, self.URLAtIndexPath(indexPath))
+        
+        return tableViewCell
+    }
+}
--- Delta/GamesViewController.swift
+++ Delta/GamesViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class GamesViewController: UITableViewController
 {
+    let directoryContentsDataSource: DirectoryContentsDataSource?
+    
+    override init(style: UITableViewStyle)
+    {
+        let error: NSError? = nil;
+        let documentsDirectoryURL = NSFileManager.defaultManager().URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask).first as! NSURL
+        
+        self.directoryContentsDataSource = DirectoryContentsDataSource(directoryURL: documentsDirectoryURL)
+        
+        super.init(style: style)
+    }
+
+    required init(coder aDecoder: NSCoder)
+    {
+        let error: NSError? = nil;
+        let documentsDirectoryURL = NSFileManager.defaultManager().URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask).first as! NSURL
+        
+        self.directoryContentsDataSource = DirectoryContentsDataSource(directoryURL: documentsDirectoryURL)
+        
+        super.init(coder: aDecoder)
+    }
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.dataSource = self.directoryContentsDataSource
+        
+        self.directoryContentsDataSource!.contentsUpdateHandler = {
+            dispatch_async(dispatch_get_main_queue(), {
+                self.tableView.reloadData()
+            })
+        }
+        
+        self.directoryContentsDataSource?.cellConfigurationBlock = { (cell, indexPath, URL) in
+            cell.textLabel?.text = URL.lastPathComponent
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        if self.directoryContentsDataSource == nil
+        {
+            let alertController = UIAlertController(title: "Games Directory Invalid", message: "Please ensure the current games directory exists, then restart the app.", preferredStyle: .Alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning()
