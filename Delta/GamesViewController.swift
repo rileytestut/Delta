aeed83fca59f7422cf2772d1eef72b4c944054cc
Riley Testut <Riley@Rileys-MacBook-Pro.local>
2015-03-08 00:50:48 -0800

    Initial Commit

---
+++ Delta/GamesViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GamesViewController.swift
+//  Delta
+//
+//  Created by Riley Testut on 3/8/15.
+//  Copyright (c) 2015 Riley Testut. All rights reserved.
+//
+
+import UIKit
+
+class GamesViewController: UITableViewController
+{
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
