c6875c44b637af00c71480d467a1eac1bc533e1e
Riley Testut <riley@rileytestut.com>
2017-09-28 19:23:49 -0700

    Renames Core Data model from Model to Delta

--- Delta/Database/DatabaseManager.swift
+++ Delta/Database/DatabaseManager.swift
@@ -65,7 +65,7 @@ final class DatabaseManager: NSPersistentContainer
     private init()
     {
         guard
-            let modelURL = Bundle(for: DatabaseManager.self).url(forResource: "Model", withExtension: "mom"),
+            let modelURL = Bundle(for: DatabaseManager.self).url(forResource: "Delta", withExtension: "mom"),
             let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL)
         else { fatalError("Core Data model cannot be found. Aborting.") }
         
