f560c95cc2d4d5aa0cba660cc494267b90ab4740
Riley Testut <riley@rileytestut.com>
2019-10-10 19:23:57 -0700

    Updates PauseViewController navigation bar styling for iOS 13

--- Delta/Pause Menu/PauseViewController.swift
+++ Delta/Pause Menu/PauseViewController.swift
@@ -93,6 +93,22 @@ extension PauseViewController
             self.pauseNavigationController.view.backgroundColor = UIColor.clear
             
             let gridMenuViewController = self.pauseNavigationController.topViewController as! GridMenuViewController
+            
+            if #available(iOS 13.0, *)
+            {
+                let navigationBarAppearance = self.pauseNavigationController.navigationBar.standardAppearance.copy()
+                navigationBarAppearance.backgroundEffect = UIBlurEffect(style: .dark)
+                navigationBarAppearance.backgroundColor = UIColor.black.withAlphaComponent(0.2)
+                navigationBarAppearance.shadowColor = UIColor.white.withAlphaComponent(0.2)
+                navigationBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+                self.pauseNavigationController.navigationBar.standardAppearance = navigationBarAppearance
+                
+                let transparentBarAppearance = navigationBarAppearance.copy()
+                transparentBarAppearance.backgroundColor = nil
+                transparentBarAppearance.backgroundEffect = nil
+                gridMenuViewController.navigationItem.standardAppearance = transparentBarAppearance
+            }
+            
             gridMenuViewController.items = self.pauseItems
             
         case "saveStates":
