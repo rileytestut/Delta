a4a98b3aeb146df1d34c9604a3a05e29c36f8268
Ian Clawson <ian@radiant.org>
2019-12-14 02:02:10 -0700

    adds tv scheme, sets the iOS app delegate as the starting point

---
+++ DeltaTV/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DeltaTV
+//
+//  Created by Ian Clawson on 12/14/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DeltaTV/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DeltaTV
+//
+//  Created by Ian Clawson on 12/14/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
