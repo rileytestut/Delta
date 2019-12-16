94a306b841c7cf8e7cad225eb19fcb6f8b0507f5
Ian Clawson <ian@radiant.org>
2019-12-16 13:24:13 -0700

    UIDevice+Processor.swift tv combat updates

--- Delta/Extensions/UIDevice+Processor.swift
+++ Delta/Extensions/UIDevice+Processor.swift
@@ -7,13 +7,22 @@
 //
 
 import UIKit
+#if os(iOS)
 import ARKit
+#endif
 
 extension UIDevice
 {
     var hasA9ProcessorOrBetter: Bool {
         // ARKit is only supported by devices with an A9 processor or better, according to the documentation.
         // https://developer.apple.com/documentation/arkit/arconfiguration/2923553-issupported
+        #if os(iOS)
         return ARConfiguration.isSupported
+        #elseif os(tvOS)
+        // TV HD has A8 chip, and TV 4K has A10X chip. Best to return true here for now
+        return true
+        #else
+        return false
+        #endif
     }
 }
