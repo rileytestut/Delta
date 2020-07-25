612552a07c412eb18ee603bda0dbb85e4a5f2b1e
Riley Testut <riley@rileytestut.com>
2020-07-24 20:56:41 -0700

    [DeltaMac] Adds skeleton DeltaMac target

---
+++ DeltaMac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeltaMac
+//
+//  Created by Riley Testut on 7/24/20.
+//  Copyright © 2020 Riley Testut. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeltaMac/DeltaMacApp.swift
@@ -0,0 +1,18 @@
+//
+//  DeltaMacApp.swift
+//  DeltaMac
+//
+//  Created by Riley Testut on 7/24/20.
+//  Copyright © 2020 Riley Testut. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct DeltaMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
