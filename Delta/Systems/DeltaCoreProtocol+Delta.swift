7034b1dd8a573186da1b24719e83dd5218d69d5e
Riley Testut <riley@rileytestut.com>
2021-01-19 12:49:55 -0600

    Fixes missing Fast Forward option for MelonDSDeltaCore

--- Delta/Systems/DeltaCoreProtocol+Delta.swift
+++ Delta/Systems/DeltaCoreProtocol+Delta.swift
@@ -70,8 +70,8 @@ extension DeltaCoreProtocol
         case GBA.core: return 3
         case N64.core where UIDevice.current.hasA11ProcessorOrBetter: return 3
         case N64.core where UIDevice.current.hasA9ProcessorOrBetter: return 1.5
-        case DS.core where UIDevice.current.supportsJIT: return 3
-        case DS.core where UIDevice.current.hasA11ProcessorOrBetter: return 2
+        case MelonDS.core where UIDevice.current.supportsJIT: return 3
+        case MelonDS.core where UIDevice.current.hasA11ProcessorOrBetter: return 2
         default: return 1
         }
     }
