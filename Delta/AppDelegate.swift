52a68e28ddaaf94155295ddfa205007b23bb5452
Riley Testut <riley@rileytestut.com>
2022-07-25 17:22:11 -0500

    Switches to UIScene-based app lifecycle

--- Delta/AppDelegate.swift
+++ Delta/AppDelegate.swift
@@ -114,6 +114,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     }
 }
 
+@available(iOS 13, *)
+extension AppDelegate
+{
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration
+    {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Main", sessionRole: connectingSceneSession.role)
+    }
+    
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>)
+    {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
 private extension AppDelegate
 {
     func registerCores()
