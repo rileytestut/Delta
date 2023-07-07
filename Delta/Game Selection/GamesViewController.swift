e33a7c662f57a30a0b523ef9f47e698a30d48e88
Riley Testut <riley@rileytestut.com>
2023-07-07 18:07:58 -0500

    [Delta Sync] Delays sync until after interactive Settings dismissal has completed
    Avoids false-positives when user scrolls to top of a Settings screen.

--- Delta/Game Selection/GamesViewController.swift
+++ Delta/Game Selection/GamesViewController.swift
@@ -646,7 +646,7 @@ extension GamesViewController: NSFetchedResultsControllerDelegate
 
 extension GamesViewController: UIAdaptivePresentationControllerDelegate
 {
-    func presentationControllerWillDismiss(_ presentationController: UIPresentationController)
+    func presentationControllerDidDismiss(_ presentationController: UIPresentationController)
     {
         self.sync()
     }
