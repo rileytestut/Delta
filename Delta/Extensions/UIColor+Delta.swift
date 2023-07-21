441f08821f834ef10e4f1ca75960289b3e4ef9a6
Chris Rittenhouse <dev@litritt.com>
2023-07-21 10:48:07 -0400

    Adds dynamic `themeColor` property to `UIColor` extension
    Returns the currently chosen theme color

--- Delta/Extensions/UIColor+Delta.swift
+++ Delta/Extensions/UIColor+Delta.swift
@@ -12,4 +12,47 @@ extension UIColor
 {
     static let deltaPurple = UIColor(named: "Purple")!
     static let deltaDarkGray = UIColor(named: "DarkGray")!
+    
+    static var themeColor: UIColor
+        {
+            // Get theme color based on user Theme Color options if feature is enabled
+            if Settings.experimentalFeatures.themeColor.isEnabled
+            {
+                // Use custom color if option is enabled
+                if Settings.experimentalFeatures.themeColor.useCustom
+                {
+                    guard let color = Settings.experimentalFeatures.themeColor.customColor.cgColor else { return .deltaPurple }
+                    
+                    return UIColor(cgColor: color)
+                }
+                // Use preset color if custom color option is disabled
+                else
+                {
+                    switch Settings.experimentalFeatures.themeColor.presetColor
+                    {
+                    case .pink:
+                        return UIColor.systemPink
+                    case .red:
+                        return UIColor.systemRed
+                    case .orange:
+                        return UIColor.systemOrange
+                    case .yellow:
+                        return UIColor.systemYellow
+                    case .green:
+                        return UIColor.systemGreen
+                    case .teal:
+                        return UIColor.systemTeal
+                    case .blue:
+                        return UIColor.systemBlue
+                    case .purple:
+                        return deltaPurple
+                    }
+                }
+            }
+            // Use default app accent color if Theme Color feature is disabled
+            else
+            {
+                return deltaPurple
+            }
+        }
 }
