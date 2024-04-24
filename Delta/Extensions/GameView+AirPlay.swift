070dd46e89b4351e7ff06b9b5f5ceae489dde1e1
Riley Testut <riley@rileytestut.com>
2024-04-24 15:29:15 -0500

    Fixes minor AirPlay message layout issues

--- Delta/Extensions/GameView+AirPlay.swift
+++ Delta/Extensions/GameView+AirPlay.swift
@@ -50,6 +50,7 @@ private extension GameView
         placeholderView.textLabel.font = UIFont.preferredFont(forTextStyle: .headline)
         placeholderView.textLabel.text = NSLocalizedString("AirPlay", comment: "")
         placeholderView.textLabel.textColor = .systemGray
+        placeholderView.textLabel.numberOfLines = 1 // Enforce single line
         
         placeholderView.detailTextLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
         placeholderView.detailTextLabel.text = NSLocalizedString("This game is playing on a second screen.", comment: "")
@@ -62,6 +63,12 @@ private extension GameView
         placeholderView.imageView.tintColor = .systemGray
         
         self.addSubview(placeholderView, pinningEdgesWith: .zero)
+        
+        // Ensure label goes to edge before wrapping to new line.
+        NSLayoutConstraint.activate([
+            placeholderView.detailTextLabel.leadingAnchor.constraint(equalTo: placeholderView.layoutMarginsGuide.leadingAnchor),
+            placeholderView.detailTextLabel.trailingAnchor.constraint(equalTo: placeholderView.layoutMarginsGuide.trailingAnchor),
+        ])
         
         self.airPlayView = placeholderView
     }
