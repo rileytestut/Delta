3795451921ecc842e238d48cb01a4eb23db550fa
Riley Testut <riley@rileytestut.com>
2024-10-09 17:55:04 -0500

    Supports Nintendo WFC for DS games
    # Conflicts:
    #	Cores/MelonDSDeltaCore

---
+++ Pods/Headers/Private/MelonDSDeltaCore/melonDS/src/frontend/libslirp/glib/glib.h
@@ -0,0 +1,1 @@
+../../../../../../../../../Cores/MelonDSDeltaCore/melonDS/src/frontend/libslirp/glib/glib.h
---
+++ Pods/Headers/Private/MelonDSDeltaCore/melonDS/src/frontend/libslirp/src/tcp_var.h
@@ -0,0 +1,1 @@
+../../../../../../../../../Cores/MelonDSDeltaCore/melonDS/src/frontend/libslirp/src/tcp_var.h
